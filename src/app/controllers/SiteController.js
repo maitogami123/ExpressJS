10b4d5ba3b2875585811638afb3875878aa8017e
Shikigami Maito <shikigamimaito25@gmail.com>
2022-09-11 16:52:21 +0700

    install prettier husky and lint-staged for code formatting

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,17 +1,14 @@
-
 class SiteController {
-    
     // [GET] /
     index(req, res) {
-        res.render('home')
+        res.render('home');
     }
 
-    // [GET] /search    
+    // [GET] /search
     // ":slug" -> là một biến có thể nhận về nhiều giá trị khác nhau
     show(req, res) {
-        res.render('search')
-    } 
-
+        res.render('search');
+    }
 }
 
-module.exports = new SiteController
+module.exports = new SiteController();
--- src/index.js
+++ src/index.js
@@ -1,30 +1,32 @@
-const path = require('path')
-const express = require('express')
-const morgan = require('morgan')
-const { engine } = require('express-handlebars')
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const { engine } = require('express-handlebars');
 
-const route = require('./routes')
+const route = require('./routes');
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.urlencoded({
-  extended: true,
-}))
-app.use(express.json())
+app.use(
+    express.urlencoded({
+        extended: true,
+      }),
+  );
+  app.use(express.json());
 
-// HTTP logger
-// app.use(morgan('combined'))
+  // HTTP logger
+  // app.use(morgan('combined'))
 
-// Template engine
-app.engine('.hbs', engine({extname: '.hbs'}));
-app.set('view engine', '.hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
+  // Template engine
+  app.engine('.hbs', engine({ extname: '.hbs' }));
+  app.set('view engine', '.hbs');
+  app.set('views', path.join(__dirname, 'resources/views'));
 
-route(app)
+  route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+      console.log(`Example app listening on port ${port}`);
+});
